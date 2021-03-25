f6b4ec74e50e79caaab1bd3fc7623456352013b8
Caffrey <447165738@qq.com>
2021-03-25 20:36:19 +0800

    提交初始项目

---
+++ GameBasicProject/Source/GameBasicProject/GameBasicProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameBasicProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEBASICPROJECT_API AGameBasicProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
